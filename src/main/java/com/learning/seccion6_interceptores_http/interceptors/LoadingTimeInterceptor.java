a75968a04d7e0d48558e93acd419898d3b8e7dfd
Moises Silva Cardenas <moises.silva@et.bo>
2024-07-18 16:13:19 -0400

    96. Retornando false en un interceptor

--- src/main/java/com/learning/seccion6_interceptores_http/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/learning/seccion6_interceptores_http/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,6 @@
 package com.learning.seccion6_interceptores_http.interceptors;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
@@ -10,6 +11,9 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 @Component("timeInterceptor")
@@ -27,6 +31,18 @@ public class LoadingTimeInterceptor implements HandlerInterceptor {
         Random random = new Random();
         int delay = random.nextInt(500);
         Thread.sleep(delay);
+        if (1>2){
+            Map<String,String> map = new HashMap<String,String>();
+            map.put("message", "Acceso no autorizado");
+            map.put("date", new Date().toString());
+            map.put("status","401 Unauthorized");
+            ObjectMapper mapper = new ObjectMapper();
+            String jsonString = mapper.writeValueAsString(map);
+            response.setContentType("application/json");
+            response.setStatus(401);
+            response.getWriter().write(jsonString);
+            return false;
+        }
         return true;
     }
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
