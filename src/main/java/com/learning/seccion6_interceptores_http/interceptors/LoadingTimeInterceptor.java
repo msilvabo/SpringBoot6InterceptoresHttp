81fd6d0284e7e70ae233b027f52df5c7763c6296
Moises Silva Cardenas <moises.silva@et.bo>
2024-07-18 15:26:55 -0400

    93. Implementando Rest Controller

--- src/main/java/com/learning/seccion6_interceptores_http/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/learning/seccion6_interceptores_http/interceptors/LoadingTimeInterceptor.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.LoggerFactoryFriend;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,12 +17,14 @@ public class LoadingTimeInterceptor implements HandlerInterceptor {
 
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
-        logger.info("LoadingTimeInterceptor: postHandle()  saliendo....");
+        HandlerMethod controller = ((HandlerMethod)handler);
+        logger.info("LoadingTimeInterceptor: preHandle()  saliendo...." + controller.getMethod().getName() );
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logger.info("LoadingTimeInterceptor: preHandle()  entrando....");
+        HandlerMethod controller = ((HandlerMethod)handler);
+        logger.info("LoadingTimeInterceptor: preHandle()  entrando...." + controller.getMethod().getName() );
         return true;
     }
 }
